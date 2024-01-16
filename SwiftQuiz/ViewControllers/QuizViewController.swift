b84f535a002898a672965fac3207e8c7f5b0145b
Konstantin <diel404@gmail.com>
2024-01-15 19:45:45 -0800

    implementation of test logic

---
+++ SwiftQuiz/ViewControllers/QuizViewController.swift
@@ -0,0 +1,94 @@
+//
+//  QuizViewController.swift
+//  SwiftQuiz
+//
+//  Created by horze on 15.01.2024.
+//
+
+import UIKit
+
+final class QuizViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    // MARK: - Private Properties
+    
+    private var questionIndex = 0
+    private let questions = Question.getQuestions()
+    private var selectedAnswer: [Int] = []
+    private var correctAnswersCount = 0
+    
+    private var totalProgress: Float {
+        Float(questionIndex) / Float(questions.count)
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showResults" {
+            let resultsViewController = segue.destination as! ResultsViewController
+            resultsViewController.correctAnswersCount = correctAnswersCount
+            resultsViewController.totalQuestionsCount = questions.count
+        }
+    }
+    
+    // MARK: - UI Updates
+    
+    private func updateUI() {
+        // Set current question
+        let currentQuestion = self.questions[self.questionIndex]
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        self.questionLabel.text = currentQuestion.text
+        self.questionProgressView.setProgress(self.totalProgress, animated: true)
+        
+        // Set titles for answer buttons
+        for (buttonIndex, button) in self.answerButtons.enumerated() {
+            if buttonIndex < currentQuestion.options.count {
+                button.setTitle(currentQuestion.options[buttonIndex], for: .normal)
+                button.isHidden = false
+            } else {
+                button.isHidden = true
+            }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        // Check if tapped button is correct
+        let currentQuestion = questions[questionIndex]
+        if let selectedIndex = answerButtons.firstIndex(of: sender) {
+            selectedAnswer.append(selectedIndex)
+            if selectedIndex == currentQuestion.correctAnswerIndex {
+                correctAnswersCount += 1
+            } else {
+                print("Неверно")
+            }
+        }
+           
+        // Proceed to the next question
+        questionIndex += 1
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            // All questions have been answered
+            finishQuiz()
+        }
+    }
+    
+    private func finishQuiz() {
+        performSegue(withIdentifier: "showResults", sender: nil)
+    }
+}
+
+
---
+++ SwiftQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  SwiftQuiz
+//
+//  Created by horze on 15.01.2024.
+//
+
+import UIKit
+
+final class ResultsViewController: UIViewController {
+    
+    @IBOutlet var shortResultLabel: UILabel!
+    @IBOutlet var backButton: UIButton!
+    
+    var correctAnswersCount = 0
+    var totalQuestionsCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shortResultLabel.text = "Вы ответили на \(correctAnswersCount) из \(totalQuestionsCount) вопросов"
+        navigationItem.hidesBackButton = true
+
+        
+    }
+
+}
